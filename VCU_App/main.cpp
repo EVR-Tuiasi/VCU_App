d31c48b26c9d9bb7e278dce8604b924d5148fbfd
alexandra-biancahilbert-05 <alexandra-bianca.hilbert@student.tuiasi.ro>
2026-03-28 04:36:31 +0200

    backend I

--- VCU_App/main.cpp
+++ VCU_App/main.cpp
@@ -54,31 +54,136 @@ int main(int argc, char *argv[])
 
 void SimulatedData_Update(void)
 {//this is called by the data acquisition thread. This should create new data and save it in the global structures with the CarData_SetValue function
-    static uint32_t AcceleratorSensor1Voltage = 0U, AcceleratorSensor1TravelPercentage = 0U, HighestCellTemperature = 0U;
+    static uint32_t AcceleratorSensor1Voltage = 0U, AcceleratorSensor2Voltage = 0U, AcceleratorSensor1TravelPercentage = 0U, AcceleratorSensor2TravelPercentage = 0U;
+    static uint32_t BrakeSensor1Voltage = 0U, BrakeSensor2Voltage = 0U, BrakeSensor1TravelPercentage = 0U, BrakeSensor2TravelPercentage = 0U;
+    static uint32_t PressureSensorVoltage = 0U, PressureSensorBars = 0U;
+    static uint32_t MedianCellTemperature = 0U, HighestCellTemperature = 0U, LowestCellTemperature = 0U, MedianCellVoltage = 0U, HighestCellVoltage = 0U, LowestCellVoltage = 0U, OverallVoltage = 0U, OverallCurrent = 0U;
+
+
 
     //STEP 1: incrementing the values
     AcceleratorSensor1Voltage += 10U;//increment by whatever value desired
+    AcceleratorSensor2Voltage += 10U;
     AcceleratorSensor1TravelPercentage += 1U;
+    AcceleratorSensor2TravelPercentage += 1U;
+    BrakeSensor1Voltage += 10U;
+    BrakeSensor2Voltage += 10U;
+    BrakeSensor1TravelPercentage += 1U;
+    BrakeSensor2TravelPercentage += 1U;
+    PressureSensorVoltage += 10U;
+    PressureSensorBars += 1U;
+
+    MedianCellTemperature += 1U;
     HighestCellTemperature += 1U;
+    LowestCellTemperature += 1U;
+    MedianCellVoltage += 1U;
+    HighestCellVoltage += 1U;
+    LowestCellVoltage += 1U;
+    OverallVoltage += 1U;
+    OverallCurrent += 1U;
 
     //STEP 2: checking if the values are valid
     if(AcceleratorSensor1Voltage > 16383U)
     {//bounds checking so we do not set values outside the allowed ones
         AcceleratorSensor1Voltage -= 16384U;
     }
+    if(AcceleratorSensor2Voltage >16383U)
+    {
+        AcceleratorSensor2Voltage -= 16384U;
+    }
     if(AcceleratorSensor1TravelPercentage > 100U)
     {
         AcceleratorSensor1TravelPercentage -= 100U;
     }
+    if(AcceleratorSensor2TravelPercentage > 100U)
+    {
+        AcceleratorSensor2TravelPercentage -= 100U;
+    }
+    if(BrakeSensor1Voltage > 16383U)
+    {
+        BrakeSensor1Voltage -= 16384U;
+    }
+    if(BrakeSensor2Voltage >16383U) /* 14 bits, 0-16383, 0 to 5.00 Volts */
+    {
+        BrakeSensor2Voltage -= 16384U;
+    }
+    if(BrakeSensor1TravelPercentage > 100U)
+    {
+        BrakeSensor1TravelPercentage -= 100U;
+    }
+    if(BrakeSensor2TravelPercentage > 100U)
+    {
+        BrakeSensor2TravelPercentage -= 100U;
+    }
+    if(PressureSensorVoltage > 500U)
+    {
+        PressureSensorVoltage -= 501U;
+    }
+    if(PressureSensorBars > 255U)
+    {
+        PressureSensorBars -= 256U;
+    }
+
+    if(MedianCellTemperature > 1023U)
+    {
+        MedianCellTemperature -= 1024U;
+    }
     if(HighestCellTemperature > 1023U)
     {
-        HighestCellTemperature -= 1023U;
+        HighestCellTemperature -= 1024U;
     }
+    if(LowestCellTemperature > 1023U)
+    {
+        LowestCellTemperature -= 1024U;
+    }
+    if(MedianCellVoltage > 1023U)
+    {
+        MedianCellVoltage -= 1024U;
+    }
+    if(HighestCellVoltage > 1023U)
+    {
+        HighestCellVoltage -= 1024U;
+    }
+    if(LowestCellVoltage > 1023U)
+    {
+        LowestCellVoltage -= 1024U;
+    }
+    if(OverallVoltage > 2047U)
+    {
+        OverallVoltage -= 2048U;
+    }
+    if(OverallCurrent > 8095U)
+    {
+        OverallCurrent -= 8096U;
+    }
+
 
     //STEP 3: setting the new values
     CarData_SetValue(PEDALS_AcceleratorSensor1Voltage, AcceleratorSensor1Voltage);
+    CarData_SetValue(PEDALS_AcceleratorSensor2Voltage, AcceleratorSensor2Voltage);
     CarData_SetValue(PEDALS_AcceleratorSensor1TravelPercentage, AcceleratorSensor1TravelPercentage);
+    CarData_SetValue(PEDALS_AcceleratorSensor2TravelPercentage, AcceleratorSensor2TravelPercentage);
+    CarData_SetValue(PEDALS_BrakeSensor1Voltage, BrakeSensor1Voltage);
+    CarData_SetValue(PEDALS_BrakeSensor2Voltage, BrakeSensor2Voltage);
+    CarData_SetValue(PEDALS_BrakeSensor1TravelPercentage, BrakeSensor1TravelPercentage);
+    CarData_SetValue(PEDALS_BrakeSensor2TravelPercentage, BrakeSensor2TravelPercentage);
+    CarData_SetValue(PEDALS_PressureSensorVoltage, PressureSensorVoltage);
+    CarData_SetValue(PEDALS_PressureSensorBars, PressureSensorBars);
+
+
+    CarData_SetValue(TSAC_MedianCellTemperature, MedianCellTemperature);
     CarData_SetValue(TSAC_HighestCellTemperature, HighestCellTemperature);
+    CarData_SetValue(TSAC_LowestCellTemperature, LowestCellTemperature);
+    CarData_SetValue(TSAC_MedianCellVoltage, MedianCellVoltage);
+    CarData_SetValue(TSAC_HighestCellVoltage, HighestCellVoltage);
+    CarData_SetValue(TSAC_LowestCellVoltage, LowestCellVoltage);
+    CarData_SetValue(TSAC_OverallVoltage, OverallVoltage);
+    CarData_SetValue(TSAC_OverallCurrent, OverallCurrent);
+
+
+
+
+
 
 }
 
--- VCU_App/mainwindow.cpp
+++ VCU_App/mainwindow.cpp
@@ -75,6 +75,17 @@ void GeneralTab_Update(MainWindow* window)
     char_array[6] = 0U;//needed so the "string" can be terminated
     window->ui->Accel_Volt_S1_lineEdit->setText((const QString)QString(char_array));
 
+    //PEDALS_AcceleratorSensor2Voltage
+    readValue = (CarData_ReadValue(PEDALS_AcceleratorSensor2Voltage) * 5000U) / 16383U;//this translates the value from interval 0-16383 to interval 0-5000
+    char_array[0] = '0' + readValue/1000U;
+    char_array[1] = '.';
+    char_array[2] = '0' + (readValue/100U)%10U;
+    char_array[3] = '0' +(readValue/10U)%10U;
+    char_array[4] = '0' + readValue%10U;
+    char_array[5] = 'V';
+    char_array[6] = 0U;//needed so the "string" can be terminated
+    window->ui->Accel_Volt_S2_lineEdit->setText((const QString)QString(char_array));
+
     //PEDALS_AcceleratorSensor1TravelPercentage
     readValue = CarData_ReadValue(PEDALS_AcceleratorSensor1TravelPercentage);
     index = 0U;
@@ -90,7 +101,246 @@ void GeneralTab_Update(MainWindow* window)
     char_array[index++] = 0;//needed so the "string" can be terminated
     window->ui->Accel_Travel_S1_lineEdit->setText((const QString)QString(char_array));
 
-    //TODO rest
+    //PEDALS_AcceleratorSensor2TravelPercentage
+    readValue = CarData_ReadValue(PEDALS_AcceleratorSensor2TravelPercentage);
+    index = 0U;
+    if(readValue >= 100U)
+    {
+        char_array[index++] = '0' + readValue/100U;
+    }
+    if(readValue >= 10U){
+        char_array[index++] = '0' + (readValue/10U)%10U;
+    }
+    char_array[index++] = '0' + readValue%10U;
+    char_array[index++] = '%';
+    char_array[index++] = 0;
+    window->ui->Accel_Travel_S2_lineEdit->setText((const QString)QString(char_array));
+
+    //PEDALS_BrakeSensor1Voltage
+    readValue = (CarData_ReadValue(PEDALS_BrakeSensor1Voltage)* 5000U) / 16383U;
+    char_array[0] = '0' + readValue/1000U;
+    char_array[1] = '.';
+    char_array[2] = '0' + (readValue/100U)%10U;
+    char_array[3] = '0' +(readValue/10U)%10U;
+    char_array[4] = '0' + readValue%10U;
+    char_array[5] = 'V';
+    char_array[6] = 0U;
+    window->ui->Brake_Volt_S1_lineEdit->setText((const QString)QString(char_array));
+
+    //PEDALS_BrakeSensor2Voltage
+    readValue = (CarData_ReadValue(PEDALS_BrakeSensor2Voltage)* 5000U) / 16383U; /* 14 bits, 0-16383, 0 to 5.00 Volts  +10U */
+    char_array[0] = '0' + readValue/1000U;
+    char_array[1] = '.';
+    char_array[2] = '0' + (readValue/100U)%10U;
+    char_array[3] = '0' +(readValue/10U)%10U;
+    char_array[4] = '0' + readValue%10U;
+    char_array[5] = 'V';
+    char_array[6] = 0U;
+    window->ui->Brake_Volt_S2_lineEdit->setText((const QString)QString(char_array));
+
+    //PEDALS_BrakeSensor1TravelPercentage
+    readValue = CarData_ReadValue(PEDALS_BrakeSensor1TravelPercentage);
+    index = 0U;
+    if(readValue >= 100U)
+    {
+        char_array[index++] = '0' + readValue/100U;
+    }
+    if(readValue >= 10U){
+        char_array[index++] = '0' + (readValue/10U)%10U;
+    }
+    char_array[index++] = '0' + readValue%10U;
+    char_array[index++] = '%';
+    char_array[index++] = 0;
+    window->ui->Brake_Travel_S1_lineEdit->setText((const QString)QString(char_array));
+
+    //PEDALS_BrakeSensor2TravelPercentage
+    readValue = CarData_ReadValue(PEDALS_BrakeSensor2TravelPercentage);
+    index = 0U;
+    if(readValue >= 100U)
+    {
+        char_array[index++] = '0' + readValue/100U;
+    }
+    if(readValue >= 10U){
+        char_array[index++] = '0' + (readValue/10U)%10U;
+    }
+    char_array[index++] = '0' + readValue%10U;
+    char_array[index++] = '%';
+    char_array[index++] = 0;
+    window->ui->Brake_Travel_S2_lineEdit->setText((const QString)QString(char_array));
+
+    //TSAC_HighestCellTemperature
+    readValue = CarData_ReadValue(TSAC_HighestCellTemperature);
+    index = 0U;
+    if(readValue >= 1000U)
+    {
+        char_array[index++] = '0' + readValue/1000U;
+    }
+    if(readValue >= 100U){
+        char_array[index++] = '0' + (readValue/100U)%10U;
+    }
+    char_array[index++] = '0' + (readValue/10U)%10U;
+    char_array[index++] = '.';
+    char_array[index++] = '0' + readValue%10U;
+    char_array[index++] = ' ';
+    char_array[index++] = 'C';
+    char_array[index++] = 0;
+    window->ui->General_Tsac_Temp_Highest_lineEdit->setText((const QString)QString(char_array));
+
+    //TSAC_MedianCellTemperature
+    readValue = CarData_ReadValue(TSAC_MedianCellTemperature);
+    index = 0U;
+    if(readValue >= 1000U)
+    {
+        char_array[index++] = '0' + readValue/1000U;
+    }
+    if(readValue >= 100U){
+        char_array[index++] = '0' + (readValue/100U)%10U;
+    }
+    char_array[index++] = '0' + (readValue/10U)%10U;
+    char_array[index++] = '.';
+    char_array[index++] = '0' + readValue%10U;
+    char_array[index++] = ' ';
+    char_array[index++] = 'C';
+    char_array[index++] = 0;
+    window->ui->General_Tsac_Temp_Median_lineEdit->setText((const QString)QString(char_array));
+
+    //TSAC_LowestCellTemperature
+    readValue = CarData_ReadValue(TSAC_LowestCellTemperature);
+    index = 0U;
+    if(readValue >= 1000U)
+    {
+        char_array[index++] = '0' + readValue/1000U;
+    }
+    if(readValue >= 100U){
+        char_array[index++] = '0' + (readValue/100U)%10U;
+    }
+    char_array[index++] = '0' + (readValue/10U)%10U;
+    char_array[index++] = '.';
+    char_array[index++] = '0' + readValue%10U;
+    char_array[index++] = ' ';
+    char_array[index++] = 'C';
+    char_array[index++] = 0;
+    window->ui->General_Tsac_Temp_Lowest_lineEdit->setText((const QString)QString(char_array));
+
+    //TSAC_HighestCellVoltage
+    readValue = CarData_ReadValue(TSAC_HighestCellVoltage);
+    index = 0U;
+    if(readValue >= 100U)
+    {
+        char_array[index++] = '0' + readValue/1000U;
+    }
+    if(readValue >= 10U){
+        char_array[index++] = '0' + (readValue/100U)%10U;
+    }
+    char_array[index++] = '.';
+    char_array[index++] = '0' + (readValue/10U)%10U;
+    char_array[index++] = '0' + readValue%10U;
+    char_array[index++] = ' ';
+    char_array[index++] = 'V';
+    char_array[index++] = 0;
+    window->ui->General_Tsac_Volt_Highest_lineEdit->setText((const QString)QString(char_array));
+
+    //TSAC_MedianCellVoltage
+    readValue = CarData_ReadValue(TSAC_MedianCellVoltage);
+    index = 0U;
+    if(readValue >= 100U)
+    {
+        char_array[index++] = '0' + readValue/1000U;
+    }
+    if(readValue >= 10U){
+        char_array[index++] = '0' + (readValue/100U)%10U;
+    }
+    char_array[index++] = '.';
+    char_array[index++] = '0' + (readValue/10U)%10U;
+    char_array[index++] = '0' + readValue%10U;
+    char_array[index++] = ' ';
+    char_array[index++] = 'V';
+    char_array[index++] = 0;
+    window->ui->General_Tsac_Volt_Median_lineEdit->setText((const QString)QString(char_array));
+
+    //TSAC_LowestCellVoltage
+    readValue = CarData_ReadValue(TSAC_LowestCellVoltage);
+    index = 0U;
+    char_array[0] = '0' + readValue/1000U;
+    if(readValue >= 1000U){
+        char_array[index++] = '0' + (readValue/1000U);
+    }
+    if(readValue >= 100U){
+        char_array[index++] = '0' + (readValue/100U)%10U;
+    }
+    char_array[index++] = '.';
+    char_array[index++] = '0' + (readValue/10U)%10U;
+
+    char_array[index++] = '0' + readValue%10U;
+    char_array[index++] = ' ';
+    char_array[index++] = 'V';
+    char_array[index++] = 0;
+
+    window->ui->General_Tsac_Volt_Lowest_lineEdit->setText((const QString)QString(char_array));
+
+    //TSAC_OverallVoltage
+    readValue = CarData_ReadValue(TSAC_OverallVoltage);
+    index = 0U;
+    if(readValue >= 1000U)
+    {
+        char_array[index++] = '0' + readValue/1000U;
+    }
+    if(readValue >= 100U){
+        char_array[index++] = '0' + (readValue/100U)%10U;
+    }
+    char_array[index++] = '0' + (readValue/10U)%10U;
+    char_array[index++] = '.';
+    char_array[index++] = '0' + readValue%10U;
+    char_array[index++] = ' ';
+    char_array[index++] = 'V';
+    char_array[index++] = 0;
+    window->ui->General_Tsac_V_Median_lineEdit->setText((const QString)QString(char_array));
+
+    //TSAC_OverallCurrent
+    readValue = CarData_ReadValue(TSAC_OverallCurrent);
+    index = 0U;
+    if(readValue >= 1000U)
+    {
+        char_array[index++] = '0' + readValue/1000U;
+    }
+    if(readValue >= 100U){
+        char_array[index++] = '0' + (readValue/100U)%10U;
+    }
+    char_array[index++] = '0' + (readValue/10U)%10U;
+    char_array[index++] = '.';
+    char_array[index++] = '0' + readValue%10U;
+    char_array[index++] = ' ';
+    char_array[index++] = 'A';
+    char_array[index++] = 0;
+    window->ui->General_Tsac_Current_Median_lineEdit->setText((const QString)QString(char_array));
+
+/*
+    //PEDALS_PressureSensorVoltage
+    readValue = (CarData_ReadValue(PEDALS_PressureSensorVoltage)* 5000U) / 500U; /* 9 bits, 0-500, 0 to 5.00 Volts, 0.1 Volts per bit
+    char_array[0] = '0' + readValue/1000U;
+    char_array[1] = '.';
+    char_array[2] = '0' + (readValue/100U)%10U;
+    char_array[3] = '0' +(readValue/10U)%10U;
+    char_array[4] = '0' + readValue%10U;
+    char_array[5] = 'V';
+    char_array[6] = 0U;
+    //window->ui->Brake_Volt_S2_lineEdit->setText((const QString)QString(char_array));
+
+    //PEDALS_PressureSensorBars
+    readValue = CarData_ReadValue(PEDALS_PressureSensorBars);
+    index = 0U;
+    if(readValue >= 255U)
+    {
+        char_array[index++] = '0' + readValue/255U;
+    }
+    if(readValue >= 10U){
+        char_array[index++] = '0' + (readValue/10U)%10U;
+    }
+    char_array[index++] = '0' + readValue%10U;
+    char_array[index++] = 'B';
+    char_array[index++] = 0;
+*/
+
 }
 
 void TsacTab_Update(MainWindow* window)
@@ -114,6 +364,133 @@ void TsacTab_Update(MainWindow* window)
     char_array[index++] = 'C';
     char_array[index++] = 0;//needed so the "string" can be terminated
     window->ui->Tsac_Temp_Highest_lineEdit->setText((const QString)QString(char_array));
+
+    //TSAC_MedianCellTemperature
+    readValue = CarData_ReadValue(TSAC_MedianCellTemperature);
+    index = 0U;
+    if(readValue >= 1000U)
+    {
+        char_array[index++] = '0' + readValue/1000U;
+    }
+    if(readValue >= 100U){
+        char_array[index++] = '0' + (readValue/100U)%10U;
+    }
+    char_array[index++] = '0' + (readValue/10U)%10U;
+    char_array[index++] = '.';
+    char_array[index++] = '0' + readValue%10U;
+    char_array[index++] = ' ';
+    char_array[index++] = 'C';
+    char_array[index++] = 0;
+    window->ui->Tsac_Temp_Median_lineEdit->setText((const QString)QString(char_array));
+
+    //TSAC_LowestCellTemperature
+    readValue = CarData_ReadValue(TSAC_LowestCellTemperature);
+    index = 0U;
+    if(readValue >= 1000U)
+    {
+        char_array[index++] = '0' + readValue/1000U;
+    }
+    if(readValue >= 100U){
+        char_array[index++] = '0' + (readValue/100U)%10U;
+    }
+    char_array[index++] = '0' + (readValue/10U)%10U;
+    char_array[index++] = '.';
+    char_array[index++] = '0' + readValue%10U;
+    char_array[index++] = ' ';
+    char_array[index++] = 'C';
+    char_array[index++] = 0;
+    window->ui->Tsac_Temp_Lowest_lineEdit->setText((const QString)QString(char_array));
+
+    //TSAC_HighestCellVoltage
+    readValue = CarData_ReadValue(TSAC_HighestCellVoltage);
+    index = 0U;
+    if(readValue >= 100U)
+    {
+        char_array[index++] = '0' + readValue/1000U;
+    }
+    if(readValue >= 10U){
+        char_array[index++] = '0' + (readValue/100U)%10U;
+    }
+    char_array[index++] = '.';
+    char_array[index++] = '0' + (readValue/10U)%10U;
+    char_array[index++] = '0' + readValue%10U;
+    char_array[index++] = ' ';
+    char_array[index++] = 'V';
+    char_array[index++] = 0;
+    window->ui->Tsac_Volt_Highest_lineEdit->setText((const QString)QString(char_array));
+
+    //TSAC_MedianCellVoltage
+    readValue = CarData_ReadValue(TSAC_MedianCellVoltage);
+    index = 0U;
+    if(readValue >= 100U)
+    {
+        char_array[index++] = '0' + readValue/1000U;
+    }
+    if(readValue >= 10U){
+        char_array[index++] = '0' + (readValue/100U)%10U;
+    }
+    char_array[index++] = '.';
+    char_array[index++] = '0' + (readValue/10U)%10U;
+    char_array[index++] = '0' + readValue%10U;
+    char_array[index++] = ' ';
+    char_array[index++] = 'V';
+    char_array[index++] = 0;
+    window->ui->Tsac_Volt_Median_lineEdit->setText((const QString)QString(char_array));
+
+    //TSAC_LowestCellVoltage
+    readValue = CarData_ReadValue(TSAC_LowestCellVoltage);
+    index = 0U;
+    if(readValue >= 100U)
+    {
+        char_array[index++] = '0' + readValue/1000U;
+    }
+    if(readValue >= 10U){
+        char_array[index++] = '0' + (readValue/100U)%10U;
+    }
+    char_array[index++] = '.';
+    char_array[index++] = '0' + (readValue/10U)%10U;
+    char_array[index++] = '0' + readValue%10U;
+    char_array[index++] = ' ';
+    char_array[index++] = 'V';
+    char_array[index++] = 0;
+    window->ui->Tsac_Volt_Lowest_lineEdit->setText((const QString)QString(char_array));
+
+    //TSAC_OverallVoltage
+    readValue = CarData_ReadValue(TSAC_OverallVoltage);
+    index = 0U;
+    if(readValue >= 1000U)
+    {
+        char_array[index++] = '0' + readValue/1000U;
+    }
+    if(readValue >= 100U){
+        char_array[index++] = '0' + (readValue/100U)%10U;
+    }
+    char_array[index++] = '0' + (readValue/10U)%10U;
+    char_array[index++] = '.';
+    char_array[index++] = '0' + readValue%10U;
+    char_array[index++] = ' ';
+    char_array[index++] = 'V';
+    char_array[index++] = 0;
+    window->ui->Tsac_V_Median_lineEdit->setText((const QString)QString(char_array));
+
+    //TSAC_OverallCurrent
+    readValue = CarData_ReadValue(TSAC_OverallCurrent);
+    index = 0U;
+    if(readValue >= 1000U)
+    {
+        char_array[index++] = '0' + readValue/1000U;
+    }
+    if(readValue >= 100U){
+        char_array[index++] = '0' + (readValue/100U)%10U;
+    }
+    char_array[index++] = '0' + (readValue/10U)%10U;
+    char_array[index++] = '.';
+    char_array[index++] = '0' + readValue%10U;
+    char_array[index++] = ' ';
+    char_array[index++] = 'A';
+    char_array[index++] = 0;
+    window->ui->Tsac_Current_Median_lineEdit->setText((const QString)QString(char_array));
+
     //TODO rest
 }
 
